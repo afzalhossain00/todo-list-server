076d6fdb4202a4598acdfc4be88ae6a969279538
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-12-29 14:05:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqk1vsl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('ToDo list server is running')
+})
+
+app.listen(port, () => console.log(`ToDo list running on ${port}`));
